668086053754ca77d09274dd31ee4fd8f2b0dc81
Japneet Kalkat <jkalkat@amstedrail.com>
2025-01-01 17:06:18 -0400

    Add dynamic login form and configure Tailwind styling

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  @Output() loginData = new EventEmitter();
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.loginData.emit(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+
+    console.log(this.loginForm.value);
+  }
 }
